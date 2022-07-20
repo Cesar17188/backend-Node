a0f8f272e6f48428702ee5d78f077c77851b4195
cesareli17188 <cesareli17188@gmail.com>
2022-07-20 18:52:41 -0500

    leer las cabeceras

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
-router.get('/message', (_req, res) => {
+
+router.get('/message', (req, res) => {
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado",
+    });
     res.send('Lista de mensajes');
 });
 
-router.post('/message', (_req, res) => {
-    res.send('Mensaje añadido');
+router.post('/message', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Mensaje "' + req.body.text + '" añadido correctamente');
 });
 
-// app.use('/', (_req, res)=>{
-//     res.send('Hola bienvenido!!');
-// });
 
 app.listen(3000);
 console.log('La aplicación esta escuchando en http://localhost:3000');
