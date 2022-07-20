2e36912db28007368b5794533fd20552af84408c
cesareli17188 <cesareli17188@gmail.com>
2022-07-20 18:28:09 -0500

    peticiones desde router y instalación nodemon

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 
 const app = express();
 
-app.use('/', (_req, res)=>{
-    res.send('Hola');
+app.use(router);
+
+router.get('/message', (_req, res) => {
+    res.send('Lista de mensajes');
 });
+
+router.post('/message', (_req, res) => {
+    res.send('Mensaje añadido');
+});
+
+// app.use('/', (_req, res)=>{
+//     res.send('Hola bienvenido!!');
+// });
 
 app.listen(3000);
 console.log('La aplicación esta escuchando en http://localhost:3000');
