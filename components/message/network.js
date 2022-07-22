f5079c9aebebdc77269340cc7d08f736da806639
cesareli17188 <cesareli17188@gmail.com>
2022-07-22 18:28:03 -0500

    controladores definiendo la lógica de negocio

--- components/message/network.js
+++ components/message/network.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const response = require('../../network/response');
+const controller = require('./controller');
 const router = express.Router();
 
 
@@ -12,13 +13,33 @@ router.get('/', (req, res) => {
     response.success(req, res, 'Lista de mensajes');
 });
 
-router.post('/', (req, res) => {
-    console.log(req.query);
-    if(req.query.error == "ok") {
-        response.error(req, res, 'Error inesperado', 500, 'Es solo una simulación de los errores');
-    } else {
-        response.success(req, res, 'Creado corretamente', 201);
-    }
+router.post("/", async (req, res) => {
+  try {
+    const body = await controller.addMessage(req.body.user, req.body.message);
+    
+    await response.success(
+      req,
+      res,
+      {
+        error: null,
+        body,
+      },
+      201
+    );
+  } catch (error) {
+    console.error(`[POST/message]: ${error}`);
+    await response.error(
+      req,
+      res,
+      {
+        error: "Internal Error Services",
+        body: null,
+      },
+      500,
+      "Es solo una simulación de los errores"
+    );
+  }
+  
 });
 
 module.exports = router;
