248354d35b111f35d6243b309cc235c608a780fb
cesareli17188 <cesareli17188@gmail.com>
2022-07-27 17:42:58 -0500

    mongoDB almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,25 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+require('dotenv').config();
+// mongodb+srv://cesareli17188:hAPtCwUFeMo6jykw@cluster0.gxkowbp.mongodb.net/telegram
+
+db.Promise = global.Promise;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+db.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+console.log('[db] Conectada con éxito')
 
 function addMessage(message) {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    return await Model.find();
 }
 
 module.exports = {
