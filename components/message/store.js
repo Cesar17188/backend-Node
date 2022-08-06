a8f5705cd771e9295a1db320b1c212d2b090842f
cesareli17188 <cesareli17188@gmail.com>
2022-08-06 18:34:38 -0500

    relacionando las entidades

--- components/message/store.js
+++ components/message/store.js
@@ -12,11 +12,22 @@ function addMessage(message) {
 }
 
 async function getMessages(filterUser) {
-    let filter = {};
-    if(filterUser !== null) {
-        filter = {user: filterUser};
-    }
-    return await Model.find(filter);
+    return new Promise((resolve, reject) => {
+        let filter = {};
+        if(filterUser !== null) {
+            filter = {user: filterUser};
+        }
+        Model.find(filter)
+        .populate('user')
+        .exec((error, populated) => {
+            if (error) {
+                reject(error);
+                return false;
+            }
+
+            resolve(populated);
+        });
+    });
 }
 
 async function removeMessage(id) {
