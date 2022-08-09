d8df45b9633c30f96e676334700e375b77631886
cesareli17188 <cesareli17188@gmail.com>
2022-08-09 18:13:23 -0500

    conectar la API al servidor de websockets

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+
 const bodyParser = require('body-parser');
-
+const socket = require('./socket');
 const db = require('./db');
-
 const router = require('./network/routes');
 
 const url_db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
 db(url_db);
 
-const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+socket.connect(server);
 
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('La aplicación esta escuchando en http://localhost:3000');
+
+server.listen(3000, function(){
+    console.log('La aplicación esta escuchando en http://localhost:3000');
+});
